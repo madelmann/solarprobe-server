b6fb797144d92be90237dfbd4c42121378e8cc95
madelmann <no-reply@github.com>
2026-01-24 19:18:58 +0100

    updated environment script to set SolarProbe server

--- prepare.sh
+++ prepare.sh
@@ -2,11 +2,14 @@ echo "Preparing environment..."
 
 BASE=$(pwd)
 
-export BASE
+read -p "Server: " SERVER
 
+export BASE
+export SERVER
+
+envsubst < ${BASE}/client/client.conf.tmp   > ${BASE}/client/client.conf
 envsubst < ${BASE}/config/base.conf.tmp     > ${BASE}/config/base.conf
 envsubst < ${BASE}/config/probes.conf.tmp   > ${BASE}/config/probes.conf
-
 
 echo "Done preparing environment."
 
